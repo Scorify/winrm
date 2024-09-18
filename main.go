cd52f4dbfba547a065ecf5c1984b529dd78085d5
1nv8rZim <msf9542@rit.edu>
2024-09-17 20:44:36 -0400

    implement scorecheck

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package winrm
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/masterzen/winrm"
 )
 
 type Schema struct {
@@ -22,6 +27,42 @@ func Run(ctx context.Context, config string) error {
 	err := json.Unmarshal([]byte(config), &schema)
 	if err != nil {
 		return err
+	}
+
+	deadline, ok := ctx.Deadline()
+	if !ok {
+		return fmt.Errorf("failed to get context deadline")
+	}
+
+	timeout := time.Until(deadline)
+
+	endpoint := winrm.NewEndpoint(
+		schema.Target,
+		schema.Port,
+		schema.HTTPS,
+		schema.Insecure,
+		[]byte{},
+		[]byte{},
+		[]byte{},
+		timeout,
+	)
+
+	client, err := winrm.NewClient(endpoint, schema.Username, schema.Password)
+	if err != nil {
+		return fmt.Errorf("failed to create client: %v", err)
+	}
+
+	stdout, stderr, _, err := client.RunCmdWithContext(ctx, schema.Command)
+	if err != nil {
+		return fmt.Errorf("failed to run command: %v", err)
+	}
+
+	if stderr != "" {
+		return fmt.Errorf("command returned error: %s", stderr)
+	}
+
+	if strings.TrimSpace(stdout) != strings.TrimSpace(schema.ExpectedOutput) {
+		return fmt.Errorf("expected output does not match actual output: %q != %q", schema.ExpectedOutput, stdout)
 	}
 
 	return nil
